bdcf5619f8689c5346f9d7ae5d7578341ba3c9cd
Ariosky Areces Gonzalez <48639831+arioskyag@users.noreply.github.com>
2022-12-24 15:47:35 +0800

    Updating structure

--- script_CHBM.sh
+++ script_CHBM.sh
@@ -5,15 +5,17 @@ echo $tittle
 echo ""
 echo "->> Starting process..."
 
-anat_path="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOKSY/realese_2/ds_bids_cbm_loris_012720/"
+anat_path="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOSKY/Data_report/DS_BIDS_CHBM/"
 echo "->> Anat Path: "$anat_path
-freesurfer_path="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOKSY/realese_2/freesurfer/"
+freesurfer_path="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOSKY/Data_report/DS_BIDS_CHBM/derivatives/freesurfer/"
 echo "->> Freesurfer OutPut Path: "$freesurfer_path
-ciftify_work_dir="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOKSY/realese_2/ciftify/"
+ciftify_work_dir="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOSKY/Data_report/DS_BIDS_CHBM/derivatives/ciftify/"
 echo "->> Ciftify OutPut Path: "$ciftify_path
+bet_work_dir="/data3_260T/data/CCLAB_DATASETS/CHBM/CHBM_ARIOSKY/Data_report/DS_BIDS_CHBM/derivatives/FSL_Bet/"
+echo "-->> FSL Bet output Path: "$bet_work_dir
 
 export $freesurfer_path
-
+# Running freesurfer
 for subject in "$anat_path"/*
 do
   if [ -d "$subject" ]; then
@@ -32,7 +34,7 @@ do
   fi
 done
 wait
-
+# Running ciftify
 for subject_freesf in "$freesurfer_path"/*
 do
 	if [ -d "$subject_freesf" ]; then
@@ -41,7 +43,35 @@ do
 		ciftify_recon_all --verbose --surf-reg FS --resample-to-T1w32k --fs-subjects-dir $freesurfer_path --ciftify-work-dir $ciftify_work_dir $subject_name
 	fi
 done
+wait
+# Running ciftify qc
+for subject in "$ciftify_work_dir"/*
+do
+  if [ -d "$subject" ]; then
 
+	subject_dir=`dirname $subject`
+	subject_name=`basename $subject`
+	echo "-->> Processing subject: "$subject_name
+	cifti_vis_recon_all subject --ciftify-work-dir=$ciftify_work_dir $subject_name
+  fi
+done
+wait
+# Running FSL bet
+for subject in "$ciftify_path"/*
+do
+  if [ -d "$subject" ]; then
+
+	subject_dir=`dirname $subject`
+	subject_name=`basename $subject`
+	echo "-->> Processing subject: "$subject_name
+ 
+  if [ -f $subject_dir"/"$subject_name"/T1w/T1w.nii.gz" ]; then
+    mkdir $bet_work_dir"/"$subject_name
+     bet $subject_dir"/"$subject_name"/T1w/T1w.nii.gz" $bet_work_dir"/"$subject_name"/"$subject_name -A -v
+  fi
+fi
+done
+wait
 echo ""
 echo "Done.."
 echo ""
